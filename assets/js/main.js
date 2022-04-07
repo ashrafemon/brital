3647a30c96a8921ba80e04253ca88bacca157098
Ashraf Emon <ashraf.emon143@gmail.com>
2022-04-07 15:48:56 +0600

    Header Logo Center

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$("#zeggenCarousel").owlCarousel({
+  items: 2,
+  loop: false,
+  autoplay: true,
+  margin: 50,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    992: {
+      items: 2,
+    },
+  },
+  nav: true,
+});
